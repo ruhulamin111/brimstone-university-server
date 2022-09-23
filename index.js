cd3d977cedd4291051c1ea82b3e47dbedb319bcc
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-09-23 09:48:13 +0600

    mongodb connect

---
+++ index.js
@@ -0,0 +1,36 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+const app = express()
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('brimstone university server')
+})
+app.listen(port, () => {
+    console.log('brimstone server running');
+})
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zjrcntk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('database connected');
+        const facultyCollection = client.db('brimstone').collection('faculty')
+
+
+
+
+    } catch (error) {
+        console.log(error);
+    } finally {
+
+    }
+}
+run().catch(console.dir)
